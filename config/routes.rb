a8de6548703357a5cc78c22c01cedf3d80a83638
Adrianna Valdivia <adrianna.valdivia@gmail.com>
2015-04-20 12:02:24 -0400

    adding a new controller and new route for voting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :topics
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  resources :topics do
+  member do
+    post 'upvote'
+  end
+end
 
   # You can have the root of your site routed with "root"
   root 'topics#index'
